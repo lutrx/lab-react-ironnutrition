c367abb63cc358f7df9da97ca18fc1fbdbfe02da
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-11-16 15:06:42 +0100

    Iteration2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import foodsData from './foods.json';
+import FoodBox from './components/FoodBox.jsx'
 
 function App() {
   const [foods, setFoods] = useState(foodsData);
   return (
     <div className="App">
-    <h2>Food List</h2>
+      <h2>Food List</h2>
+      <FoodBox food={ {
+        name: "Orange",
+        calories: 85,
+        image: "https://i.imgur.com/abKGOcv.jpg",
+        servings: 1
+      }} />
       {foods.map((food, index) => {
         return (
           <div key={index}>
